c5ded52a4de5c49e395fef08e216bf6240020689
Cristobal Cornejo <cristofopo@me.com>
2021-04-17 21:07:46 -0400

    1 commit

---
+++ caso1ferreteria/caso1ferreteria/__init__.py
@@ -0,0 +1,3 @@
+"""
+Package for caso1ferreteria.
+"""
---
+++ caso1ferreteria/env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Cris20xx\source\repos\Caso1Ferreteria\caso1ferreteria\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
